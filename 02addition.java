7ba9c985f8fa26dc65af203981bd35427a4b710f
VRAJ1307 <110444992+VRAJ1307@users.noreply.github.com>
2022-08-05 11:35:33 -0700

    mistake

--- 02addition.java
+++ 02addition.java
@@ -3,7 +3,7 @@ class addition
     public static void main(String args[])
     {
       int a = 7 , b = 3 , sum; //defined values in compile time
-      sum = a = b; //formula
+      sum = a + b; //formula
         System.out.println("addition of two numbers is =" + sum); //plus sign used to concate the strings 
     }
 }
