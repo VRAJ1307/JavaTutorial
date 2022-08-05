3e89196758c794a2e79c18d64aa3bf9e1664e679
VRAJ1307 <110444992+VRAJ1307@users.noreply.github.com>
2022-08-05 11:22:03 -0700

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,7 @@
+class hello  //save filename as class name else it will give error
+{
+    public static void main(String args[])
+    {
+        System.out.println("hello java"); //used to print statement
+    }
+}
