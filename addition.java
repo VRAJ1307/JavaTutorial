aca90b663a37ecb003477e1331615cc3b61dd0a3
VRAJ1307 <110444992+VRAJ1307@users.noreply.github.com>
2022-08-05 11:31:12 -0700

    comments

--- addition.java
+++ addition.java
@@ -2,8 +2,8 @@ class addition
 {
     public static void main(String args[])
     {
-      int a = 7 , b = 3 , sum;
-      sum = a = b;
-        System.out.println("addition of two numbers is =" + sum);
+      int a = 7 , b = 3 , sum; //defined values in compile time
+      sum = a = b; //formula
+        System.out.println("addition of two numbers is =" + sum); //plus sign used to concate the strings 
     }
 }
