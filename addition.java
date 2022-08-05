58450e7a5f43e6fbf5a8b2325e842d979947c902
VRAJ1307 <110444992+VRAJ1307@users.noreply.github.com>
2022-08-05 11:27:49 -0700

    Create addition.java

---
+++ addition.java
@@ -0,0 +1,9 @@
+class addition
+{
+    public static void main(String args[])
+    {
+      int a = 7 , b = 3 , sum;
+      sum = a = b;
+        System.out.println("addition of two numbers is =" + sum);
+    }
+}
