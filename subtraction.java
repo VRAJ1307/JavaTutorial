7452629181497874f1c3a816feabf945c5605d74
VRAJ1307 <110444992+VRAJ1307@users.noreply.github.com>
2022-08-05 11:34:51 -0700

    Create subtraction.java

---
+++ subtraction.java
@@ -0,0 +1,9 @@
+class subtraction
+{
+    public static void main(String args[])
+    {
+      int  num1 = 77 , num2 = 43 , sub; //defined values in compile time
+      sub = num1 - num2; //formula
+        System.out.println("subtraction of two numbers is =" + sub); //plus sign used to concate the strings 
+    }
+}
