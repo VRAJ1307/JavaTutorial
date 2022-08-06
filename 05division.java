e989952379ede3d51e2526d745aa8c1286631eee
VRAJ1307 <110444992+VRAJ1307@users.noreply.github.com>
2022-08-05 21:07:37 -0700

    Create 05division.java

---
+++ 05division.java
@@ -0,0 +1,9 @@
+class division
+{
+    public static void main(String args[])
+    {
+      int a = 130 , b = 5 , div; //defined values in compile time
+      div = a / b; //formula
+        System.out.println("divison of two numbers is =" + div); //plus sign used to concate the strings 
+    }
+}
