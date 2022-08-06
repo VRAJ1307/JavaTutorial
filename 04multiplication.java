400bb4cc107339d9067abe35b22b056b9ae90ef9
VRAJ1307 <110444992+VRAJ1307@users.noreply.github.com>
2022-08-05 21:03:58 -0700

    Create 04multiplication.java

---
+++ 04multiplication.java
@@ -0,0 +1,9 @@
+class multiplication
+{
+    public static void main(String args[])
+    {
+      int a = 5 , b = 8 , multi; //defined values in compile time
+      multi = a * b; //formula
+        System.out.println("multiplication of two numbers is =" + multi); //plus sign used to concate the strings 
+    }
+}
